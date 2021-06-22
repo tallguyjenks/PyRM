78c9071bf6a072c4e3c4c7811c205b8bec63b9f9
Bryan.Jenks <Bryan.Jenks@covered.ca.gov>
2021-06-22 15:13:45 -0700

    👌 IMPROVE: massive clean up

--- src/module/__init__.py
+++ src/module/__init__.py
@@ -1,1 +1,5 @@
 from .module import *
+
+# if somebody does "from somepackage import *", this is what they will
+# be able to access:
+# __all__ = ["multiply"]
--- src/pyrm.py
+++ src/pyrm.py
@@ -4,11 +4,8 @@ import logger
 # This will create the log file if it doesn't yet exist
 # !The logger needs to be initialized before the other module imports
 log = logger.setup_applevel_logger(file_name="src/logs/app.log")
-import mymodule  # noqa: E402
+import module  # noqa: E402
 
 log.debug("Calling module function.")
-mymodule.multiply(5, 2)
+module.multiply(5, 2)
 log.debug("Finished.")
-
-# TODO how does __init__.py work
-# TODO importing directories and modules how work
