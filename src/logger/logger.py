8a74d03bd322cda4d298573f66fc68e7338aefa9
Bryan Jenks <bryan@bryanjenks.dev>
2021-06-24 03:24:59 -0700

    🐛 FIX: tidy up app

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -5,6 +5,7 @@ from rich.logging import RichHandler
 
 APP_LOGGER_NAME = "pyrm"
 
+
 # TODO DocString for this func
 def setup_applevel_logger(logger_name=APP_LOGGER_NAME, file_name=None):  # noqa: E302
     logger = logging.getLogger(logger_name)
@@ -30,3 +31,18 @@ def setup_applevel_logger(logger_name=APP_LOGGER_NAME, file_name=None):  # noqa:
 # TODO DocString for this func
 def get_logger(module_name):
     return logging.getLogger(APP_LOGGER_NAME).getChild(module_name)
+
+
+try:
+    with open("logs/app.log") as file:
+        _log = logging.getLogger(APP_LOGGER_NAME)
+        _log.setLevel(logging.DEBUG)
+        _shell_formatter = logging.Formatter("%(message)s")
+        _shell_handler = RichHandler()
+        _shell_handler.setFormatter(_shell_formatter)
+        _log.handlers.clear()
+        _log.addHandler(_shell_handler)
+        _log.debug("Log File Found at /logs/app.log")
+except FileNotFoundError:
+    print("Sorry the file we're looking for doesn't exist")
+    exit()
--- src/module/__init__.py
+++ src/module/__init__.py
@@ -1,5 +1,5 @@
-# from .module import *
+from .module import *
 
 # if somebody does "from somepackage import *", this is what they will
 # be able to access:
-__all__ = ["multiply"]
+# __all__ = ["multiply"]
